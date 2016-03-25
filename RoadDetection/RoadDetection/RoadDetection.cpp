d4e6af244b94f94de0515d12da1d99cd9f69a6ad
jowdemanne <stefanverhelst@gmail.com>
2016-03-25 12:28:34 +0100

    Working libraries

--- RoadDetection/RoadDetection/RoadDetection.cpp
+++ RoadDetection/RoadDetection/RoadDetection.cpp
@@ -3,11 +3,23 @@
 
 #include "stdafx.h"
 #include <iostream>
-
+#include <opencv2/opencv.hpp>
+#include <opencv\highgui.h>
+#include <iostream>
+using namespace std;
+using namespace cv;
 
 
 int main()
 {
+	String filename = "C:\\School\\5e Jaar\\Computervisie\\images\\clouds";
+	String ext = ".png";
+
+	Mat image1;	//read image
+	image1 = imread(filename + ext, CV_LOAD_IMAGE_COLOR);
+	namedWindow("window", 1);
+	imshow("window", image1);
+	waitKey();
 	std::cout << "EINDE" << std::endl;
     return 0;
 }
