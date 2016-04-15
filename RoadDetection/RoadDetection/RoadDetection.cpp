37b0bd6957810ea1e16a27c570e066634ef96958
robrechtsinnaeve <robrechtsinnaeve@msn.com>
2016-04-15 11:51:48 +0200

    Canny en mask

--- RoadDetection/RoadDetection/RoadDetection.cpp
+++ RoadDetection/RoadDetection/RoadDetection.cpp
@@ -10,15 +10,26 @@
 using namespace std;
 using namespace cv;
 
-void print_enkel_mask(string filename, string maskname, string ext) {
+void print_enkel_mask(string filename, string maskname, string ext, string number) {
 	Mat image, mask;	//read image
 	image = imread(filename + ext, CV_LOAD_IMAGE_COLOR);
 	mask = imread(maskname + ext, CV_LOAD_IMAGE_COLOR);
+	//cout << mask << endl << endl;
 	threshold(mask, mask, 1, 255, 0);
+	//imshow("window", mask);
+	//waitKey();
+	Mat contour_mask;
+	Canny(mask, contour_mask, 50, 150);
+	//imshow("window", contour_mask);
+	//waitKey();
 	bitwise_and(image, mask, image);
+	Mat contour;
+	Canny(image, contour, 50, 150);
 	namedWindow("window", 1);
-	imshow("window", image);
+	//imshow("window", image);
+	imshow("window", contour);
 	waitKey();
+	
 }
 
 string format(int num) {
@@ -29,13 +40,12 @@ string format(int num) {
 
 int main()
 {
-	for (int i = 0; i < 96; i++) {
-		string map = "02";
+	for (int i = 10; i < 106; i++) {
+		string map = "01";
 		String filename = "../../../images/" + map + "/frame" + format(i); 
 		String maskname = "../../../images/" + map + "/mask" + format(i);
 		String ext = ".png";
-
-		print_enkel_mask(filename, maskname, ext);
+		print_enkel_mask(filename, maskname, ext, format(i));
 		std::cout << "EINDE" << std::endl;
 	}
 	
