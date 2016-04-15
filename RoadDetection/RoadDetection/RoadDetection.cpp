c0207dc190e74145207a792b0a5d7e6567a80c49
robrechtsinnaeve <robrechtsinnaeve@msn.com>
2016-04-15 11:22:27 +0200

    Update RoadDetection.cpp
    Mask toegevoegd

--- RoadDetection/RoadDetection/RoadDetection.cpp
+++ RoadDetection/RoadDetection/RoadDetection.cpp
@@ -6,21 +6,41 @@
 #include <opencv2/opencv.hpp>
 #include <opencv\highgui.h>
 #include <iostream>
+#include <iomanip>
 using namespace std;
 using namespace cv;
 
+void print_enkel_mask(string filename, string maskname, string ext) {
+	Mat image, mask;	//read image
+	image = imread(filename + ext, CV_LOAD_IMAGE_COLOR);
+	mask = imread(maskname + ext, CV_LOAD_IMAGE_COLOR);
+	threshold(mask, mask, 1, 255, 0);
+	bitwise_and(image, mask, image);
+	namedWindow("window", 1);
+	imshow("window", image);
+	waitKey();
+}
+
+string format(int num) {
+	std::ostringstream oss;
+	oss << std::setfill('0') << std::setw(5) << num;
+	return oss.str();
+};
 
 int main()
 {
-	String filename = "../../../images/01/frame00000";
-	String ext = ".png";
+	for (int i = 0; i < 96; i++) {
+		string map = "02";
+		String filename = "../../../images/" + map + "/frame" + format(i); 
+		String maskname = "../../../images/" + map + "/mask" + format(i);
+		String ext = ".png";
 
-	Mat image1;	//read image
-	image1 = imread(filename + ext, CV_LOAD_IMAGE_COLOR);
-	namedWindow("window", 1);
-	imshow("window", image1);
-	waitKey();
-	std::cout << "EINDE" << std::endl;
+		print_enkel_mask(filename, maskname, ext);
+		std::cout << "EINDE" << std::endl;
+	}
+	
     return 0;
 }
+
+
 
